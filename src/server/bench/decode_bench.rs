2cc15b2b9331b2323852a0811d6709e1366a0b92
neetdai <974498346@qq.com>
2020-07-25 17:05:40 +0800

    删除无用的性能测试

--- src/server/bench/decode_bench.rs
+++
@@ -1,9 +0,0 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use ::decode::Decode;
-
-fn criterion_benchmark(c: &mut Criterion) {
-
-}
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
